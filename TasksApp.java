64a14fe4ba7bfd45d985fba66a0282936eb4461d
Aleksandra Ponochovna <47608387+AleksandraPonochovna@users.noreply.github.com>
2019-06-21 15:03:48 +0300

    Update TasksApp.java

--- TasksApp.java
+++ TasksApp.java
@@ -1,13 +1,17 @@
 package com.company;
 
-import java.io.*;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.ObjectOutputStream;
+import java.io.ObjectInputStream;
+import java.io.ByteArrayInputStream;
 import java.util.Comparator;
 import java.util.List;
 import java.util.function.BiConsumer;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
-public class TasksApp<T> {
+public class TasksApp {
 
     public static void main(String[] args) {
         //Task with comparator
@@ -15,9 +19,21 @@ public class TasksApp<T> {
         Stream stream = Stream.empty();
         BiConsumer minMaxConsumer = (s1, s2) -> System.out.println(s1 + " " + s2);
         findMinMax(stream, comparator, minMaxConsumer);
-        //Task with Serializable
-        byte[] data = {0, 1, 2, 3, 4, 56, 7, 8, 8, 8, 5};
-        deserializeAnimalArray(data);
+        //Task with Serializable for TESTS
+        Animal[] animals = {new Animal("Name"), new Animal("Name"), new Animal("Name"),
+                new Animal("Name"), new Animal("Name"), new Animal("Name")};
+        try (ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+             ObjectOutputStream objectOutputStream = new ObjectOutputStream(byteArrayOutputStream)) {
+            objectOutputStream.writeInt(animals.length);
+            for (Animal value : animals) {
+                objectOutputStream.writeObject(value);
+            }
+            for (Animal animal : deserializeAnimalArray(byteArrayOutputStream.toByteArray())) {
+                System.out.println(animal.toString());
+            }
+        } catch (IOException | NullPointerException | ClassCastException ex) {
+            throw new IllegalArgumentException("Some exception in main.");
+        }
     }
 
     public static <T> void findMinMax(
@@ -33,15 +49,17 @@ public class TasksApp<T> {
     }
 
     public static Animal[] deserializeAnimalArray(byte[] data) {
-        try (ObjectInputStream inputStream = new ObjectInputStream(new ByteArrayInputStream(data))) {
-            int size = ois.readInt();
+        try (ByteArrayInputStream byteStream = new ByteArrayInputStream(data);
+             ObjectInputStream objInputStream = new ObjectInputStream(byteStream)) {
+            int size = objInputStream.readInt();
             Animal[] animals = new Animal[size];
-            for (Animal animal : animals) {
-                animal = (Animal) ois.readObject();
+            for (int i = 0; i < animals.length; i++) {
+                animals[i] = (Animal) objInputStream.readObject();
             }
             return animals;
         } catch (IOException | NullPointerException | ClassNotFoundException | ClassCastException ex) {
-            throw new IllegalArgumentException("It's exception.");
+            throw new IllegalArgumentException("It's exception in deserializeAnimalArray ().");
         }
     }
 }
+
